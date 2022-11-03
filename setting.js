3a109ee43007fecccbb85ece94ccef9bb6263607
kaylaa-corde <116987386+kaylaa-corde@users.noreply.github.com>
2022-11-03 07:31:10 +0700

    Update setting.js

--- setting.js
+++ setting.js
@@ -10,18 +10,18 @@ global.Api = {
 global.ApiKey = 'coderevolution' //JIKA INGIN MENDAPATKAN KOMISI DAFTAR DI HTTPS://AMPIBISMM.MY.ID UNTUK MENDAPATKAN APIKEY!
 
 global.smm = {
-  nomor : '6283102650464',
-  atasnama : 'Ahmad Nur Fadhil'
+  nomor : '6285939048622',
+  atasnama : 'Kaylaa'
 }
 
 // Other
-global.owner = ['6283102650464',"6282135724604"]
-global.premium = ['6283102650464']
-global.profilebio = ['6283102650464'] // ini ganti nomor lu ntar bisa nampilin pp lu
+global.owner = ['6285939048622',"6287752099247"]
+global.premium = ['6285939048622']
+global.profilebio = ['6285939048622'] // ini ganti nomor lu ntar bisa nampilin pp lu
 global.sponsor = 'no sponsors in:('
-global.packname = 'Crizzy-Bot'
-global.footer = '© 2022 Fadhil Graphy'
-global.author = 'Owner : Fadhil Graphy\nIg1 : @fdhlgrphy\nIg2 : @nr.fdhl__'
+global.packname = 'KayyBotz'
+global.footer = '© 2022 KayyBotz'
+global.author = 'Owner : Kaylaa\nIg1 : @xolboyyy\nIg2 : gada'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.act = {
 		wait: 'please wait a moment, processing data...',
